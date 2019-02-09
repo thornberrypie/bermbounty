efb9511920267471663052d849e3d738da8eaaf2
GraemeT <gdthornber@gmail.com>
2019-02-09 23:14:36 +1300

    add map component

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Map from './components/Map';
 import Home from './views/Home';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Home />
+        <Home/>
+        <Map/>
       </View>
     );
   }
