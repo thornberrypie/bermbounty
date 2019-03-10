f7142928b06058d49dc8cd2608b112a77d4a3bc3
GraemeT <gdthornber@gmail.com>
2019-03-10 21:41:00 +1300

    Remove react-native-maps, add mapbox

--- app/views/Map.js
+++ app/views/Map.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { MapView } from 'react-native-maps';
+import { MAPBOX_API_KEY } from 'react-native-dotenv';
+//import { MapView } from 'react-native-maps';
 
 export default class Map extends React.Component {
   render() {
