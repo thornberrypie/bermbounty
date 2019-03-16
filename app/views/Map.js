c62057a48e45df12b230f6417136bece22231611
GraemeT <thornberrypie@gmail.com>
2019-03-16 21:31:56 +1300

    Add mapbox stuff

--- app/views/Map.js
+++ app/views/Map.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { MAPBOX_API_KEY } from 'react-native-dotenv';
-//import { MapView } from 'react-native-maps';
+import React, {Component} from 'react';
+import { Text, View } from 'react-native';
+import { MAPBOX_ACCESS_TOKEN } from 'react-native-dotenv';
+import MapboxGL from '@mapbox/react-native-mapbox-gl';
+
+MapboxGL.setAccessToken(MAPBOX_ACCESS_TOKEN);
+
+const columbusCircleCoordinates = [
+  -73.98197650909422, 40.768793007758816
+];
 
 export default class Map extends React.Component {
   render() {
     return (
-      <Text>Map View</Text>
-      /*
-      <MapView
-        style={{ flex: 1 }}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      />
-      */
+      <View style={{flex: 1}}>
+          <MapboxGL.MapView
+          ref={(c) => this._map = c}
+          style={{flex: 1}}
+          zoomLevel={1}>
+        </MapboxGL.MapView>
+      </View>
     );
   }
 }
